4e64d64b8f47cf5c4d7fd98f140cd8d6faa07b45
Kunal Jaiswal <kunal.jaiswal@yahoo.com>
2014-03-14 22:30:12 +0530

    Class with set and get member functions

---
+++ program_five.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+#include <string>
+
+using std::string;
+using std::getline;
+
+class GradeBook
+{
+private:
+  string courseName;
+
+public:
+
+  void setCourseName(string name){
+    courseName = name;
+  }
+
+  string getCourseName(){
+    return courseName;
+  }
+
+  void displayCourseName(){
+    cout << "Welcome to course: " << courseName << "!"<< endl;
+  }
+};
+
+int main(){
+
+  string course_name;
+  GradeBook gradebook;
+
+  cout << "Initial course name: " << gradebook.getCourseName() << endl;
+
+  cout << "Enter the course name: \n";
+  getline(cin, course_name);
+  gradebook.setCourseName(course_name);
+
+  gradebook.displayCourseName();
+
+}
